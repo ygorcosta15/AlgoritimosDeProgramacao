a64f367bab6e4066451daa4521a2e7cad40ab605
YgorCamargo <ygorcamargo55@gmail.com>
2017-04-01 15:51:56 -0300

    add o exercio 13. obs: o exercio 14 tinha cido feito em cima do exercio 13.

--- src/ApIExercicos/Ex13.java
+++ src/ApIExercicos/Ex13.java
@@ -4,23 +4,20 @@ public class Ex13 {
     public static void main(String [] args){
         Scanner leitor = new Scanner (System.in);
         
-        double v1,v2,aux;
+        int v1, v2;
         
-        System.out.println("Valor 1:  ");
-        v1 = leitor.nextDouble();
+        System.out.println("entre com o primeiro valor:   ");
+        v1 = leitor.nextInt();
         
-        System.out.println("Valor 2:");
-        v2 = leitor.nextDouble();
+        System.out.println("entre com o segundo valor:   ");
+        v2 = leitor.nextInt();
         
-        if(v1 > v2 ){
-            aux = v1/v2;
-            System.out.println("o primeiro valor dividido pelo segundo:  " + aux);
-        }else if (v1 < v2 ){
-            aux = v2/v1;
-            System.out.println("o segundo valor é dividido pelo primeiro:  " + aux);
-        }
+        if(v1>v2){
+            System.out.println("o primeiro valor é maior:  "+ v1);
             
-        
+        }else{
+            System.out.println("o segundo valor é maior:  " + v2);
+        }
         
     }
     
